6a83aabc548174e41ba32063413ee266cbd73720
Raju Kumar Raja <rajukumar.191813@gmail.com>
2025-01-02 21:44:19 +0530

    optimized reverse

--- ARRAYS/EASY/ReverseArray.java
+++ ARRAYS/EASY/ReverseArray.java
@@ -22,18 +22,22 @@ public class ReverseArray {
     }
 
     public static int[] ReverseArray(int[] arr) {
-        int left = 0, right = arr.length - 1, temp;
-
-        while (left < right) {
-            temp = arr[left];
+      
+        int left = 0 , right = arr.length-1 ;
+        
+        while (left<right) {
+            int temp = arr[left];
             arr[left] = arr[right];
             arr[right] = temp;
-
-
-            left++;
-            right--;
+    
+            left ++;
+            right --;
         }
+        return arr;
+        }
+       
 
-        return arr; 
-    }
 }
+
+
+
